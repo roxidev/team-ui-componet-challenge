94fab35aefd672506870df2ee74ccc7cf7700183
Mahbubul Alam <mahbublalam500@gmail.com>
2023-12-25 23:08:24 +0600

    Routes Update

---
+++ src/components/Clipboard/Clipboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clipboard = () => {
+  return (
+    <div>Clipboard</div>
+  )
+}
+
+export default Clipboard
---
+++ src/components/MonochromeMode/MonochromeMode.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MonochromeMode = () => {
+  return <div>MonochromeMode</div>;
+};
+
+export default MonochromeMode;
